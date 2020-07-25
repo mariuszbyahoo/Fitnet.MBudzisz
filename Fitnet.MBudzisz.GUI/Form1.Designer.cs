f3a53f8981cefc99b7ebad28d54254d9b2abdd8a
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-07-25 13:16:42 +0200

    addign split container in buttonsPanel

--- Fitnet.MBudzisz.GUI/Form1.Designer.cs
+++ Fitnet.MBudzisz.GUI/Form1.Designer.cs
@@ -35,20 +35,28 @@
             this.panel4 = new System.Windows.Forms.Panel();
             this.logsTextBox = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.executeButton = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.lookupStringTextBox = new System.Windows.Forms.TextBox();
             this.stringLabel = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pathTextBox = new System.Windows.Forms.TextBox();
             this.pathLabel = new System.Windows.Forms.Label();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.mainBackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.executeButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressPanel = new System.Windows.Forms.Panel();
+            this.buttonsPanel = new System.Windows.Forms.Panel();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.buttonsSplitter = new System.Windows.Forms.Splitter();
             this.ssMain.SuspendLayout();
             this.mainPanel.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.progressPanel.SuspendLayout();
+            this.buttonsPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // ssMain
@@ -66,8 +74,8 @@
             // 
             this.toolStripStatusLabel1.BackColor = System.Drawing.Color.Transparent;
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(192, 20);
-            this.toolStripStatusLabel1.Text = "MBudzisz +48-796-666-343";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(502, 20);
+            this.toolStripStatusLabel1.Text = "+48-796-666-343 mariusz.budzisz@yahoo.com SKYPE: live:mariusz.budzisz";
             // 
             // mainPanel
             // 
@@ -83,6 +91,7 @@
             // 
             // panel4
             // 
+            this.panel4.Controls.Add(this.button1);
             this.panel4.Controls.Add(this.logsTextBox);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel4.Location = new System.Drawing.Point(0, 250);
@@ -107,27 +116,14 @@
             // 
             // panel3
             // 
-            this.panel3.Controls.Add(this.progressBar1);
-            this.panel3.Controls.Add(this.executeButton);
+            this.panel3.Controls.Add(this.buttonsPanel);
+            this.panel3.Controls.Add(this.progressPanel);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(0, 150);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(800, 100);
             this.panel3.TabIndex = 2;
-            // 
-            // executeButton
-            // 
-            this.executeButton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.executeButton.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.executeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.executeButton.Location = new System.Drawing.Point(0, 50);
-            this.executeButton.Name = "executeButton";
-            this.executeButton.Padding = new System.Windows.Forms.Padding(3);
-            this.executeButton.Size = new System.Drawing.Size(800, 50);
-            this.executeButton.TabIndex = 0;
-            this.executeButton.Text = "ExecuteLookup";
-            this.executeButton.UseVisualStyleBackColor = true;
-            this.executeButton.Click += new System.EventHandler(this.executeButton_Click);
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             // 
             // panel2
             // 
@@ -197,13 +193,81 @@
             this.pathLabel.Text = "Enter a path to catalog below:";
             this.pathLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // progressBar1
+            // mainBackgroundWorker
             // 
-            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.progressBar1.Location = new System.Drawing.Point(0, 0);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(800, 50);
-            this.progressBar1.TabIndex = 1;
+            this.mainBackgroundWorker.WorkerReportsProgress = true;
+            this.mainBackgroundWorker.WorkerSupportsCancellation = true;
+            // 
+            // executeButton
+            // 
+            this.executeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.executeButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.executeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.executeButton.Location = new System.Drawing.Point(0, 0);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Padding = new System.Windows.Forms.Padding(3);
+            this.executeButton.Size = new System.Drawing.Size(396, 50);
+            this.executeButton.TabIndex = 0;
+            this.executeButton.Text = "ExecuteLookup";
+            this.executeButton.UseVisualStyleBackColor = true;
+            this.executeButton.Click += new System.EventHandler(this.executeButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // progressPanel
+            // 
+            this.progressPanel.Controls.Add(this.progressBar);
+            this.progressPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.progressPanel.Location = new System.Drawing.Point(0, 0);
+            this.progressPanel.Name = "progressPanel";
+            this.progressPanel.Size = new System.Drawing.Size(800, 50);
+            this.progressPanel.TabIndex = 0;
+            // 
+            // buttonsPanel
+            // 
+            this.buttonsPanel.Controls.Add(this.cancelButton);
+            this.buttonsPanel.Controls.Add(this.buttonsSplitter);
+            this.buttonsPanel.Controls.Add(this.executeButton);
+            this.buttonsPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonsPanel.Location = new System.Drawing.Point(0, 50);
+            this.buttonsPanel.Name = "buttonsPanel";
+            this.buttonsPanel.Size = new System.Drawing.Size(800, 50);
+            this.buttonsPanel.TabIndex = 1;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar.Location = new System.Drawing.Point(0, 0);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(800, 50);
+            this.progressBar.TabIndex = 0;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cancelButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cancelButton.Location = new System.Drawing.Point(399, 0);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(401, 50);
+            this.cancelButton.TabIndex = 0;
+            this.cancelButton.Text = "cancel button";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // buttonsSplitter
+            // 
+            this.buttonsSplitter.Location = new System.Drawing.Point(396, 0);
+            this.buttonsSplitter.Name = "buttonsSplitter";
+            this.buttonsSplitter.Size = new System.Drawing.Size(3, 50);
+            this.buttonsSplitter.TabIndex = 1;
+            this.buttonsSplitter.TabStop = false;
             // 
             // MainForm
             // 
@@ -224,6 +288,8 @@
             this.panel2.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.progressPanel.ResumeLayout(false);
+            this.buttonsPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -239,12 +305,18 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label pathLabel;
         private System.Windows.Forms.TextBox logsTextBox;
-        private System.Windows.Forms.Button executeButton;
         private System.Windows.Forms.TextBox lookupStringTextBox;
         private System.Windows.Forms.Label stringLabel;
         private System.Windows.Forms.TextBox pathTextBox;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
-        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker mainBackgroundWorker;
+        private System.Windows.Forms.Button executeButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel buttonsPanel;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Splitter buttonsSplitter;
+        private System.Windows.Forms.Panel progressPanel;
+        private System.Windows.Forms.ProgressBar progressBar;
     }
 }
 
--- Fitnet.MBudzisz.GUI/Form1.cs
+++ Fitnet.MBudzisz.GUI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fitnet.MBudzisz.DAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,12 +13,19 @@ namespace Fitnet.MBudzisz.GUI
 {
     public partial class MainForm : Form
     {
+        private readonly IDataInspector _inspector;
         public MainForm()
         {
+            _inspector = new HardDriveInspector();
             InitializeComponent();
         }
 
         private void executeButton_Click(object sender, EventArgs e)
+        {
+            _inspector.InspectDataLayer(this.pathTextBox.Text, this.lookupStringTextBox.Text);
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
         {
 
         }
